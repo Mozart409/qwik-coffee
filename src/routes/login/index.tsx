efdd8308e98e0d8b0df267d1fc57be85172d1f41
Amadeus <38767929+Mozart409@users.noreply.github.com>
2023-07-01 19:06:51 +0200

    feat: :sparkles: testing auth

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,18 +1,18 @@
-import { component$ } from "@builder.io/qwik";
-import { Form } from "@builder.io/qwik-city";
-import { Button } from "~/components/button/button";
-import { useAuthSignin } from "~/routes/plugin@auth";
+import { component$ } from '@builder.io/qwik'
+import { Form } from '@builder.io/qwik-city'
+import { Button } from '~/components/button/button'
+import { useAuthSignin } from '~/routes/plugin@auth'
 export default component$(() => {
-  const signIn = useAuthSignin();
+  const signIn = useAuthSignin()
   return (
     <Form action={signIn}>
       <input type="hidden" name="providerId" value="github" />
       <input
         type="hidden"
         name="options.callbackUrl"
-        value="https://qwik.consumption.coffee/about"
+        value="/api/auth/signin/github"
       />
       <Button>Sign In</Button>
     </Form>
-  );
-});
+  )
+})
